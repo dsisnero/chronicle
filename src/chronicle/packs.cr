4a8361f9562ebe89bf94a8823e0fb15eef3f84e9
Dominic Sisneros <dsisnero@gmail.com>
2026-07-31 21:03:32 -0600

    port: add pack system aggregate require (src/chronicle/packs.cr)

---
+++ src/chronicle/packs.cr
@@ -0,0 +1,16 @@
+# The pack system: bundles of object types, relation types, behaviors,
+# tools, prompts, and policies for a domain. Ported from activegraph
+# activegraph/packs/* (revision 8aedb1866cf5dce056af97529152ffd6f468a1ed).
+#
+# Pack-aware decorators in Python become Crystal annotations collected by the
+# `Chronicle::Packs::DSL.pack` macro; nothing registers globally.
+require "./packs/exceptions"
+require "./packs/prompt"
+require "./packs/settings"
+require "./packs/behavior"
+require "./packs/value_objects"
+require "./packs/annotations"
+require "./packs/discovery"
+require "./packs/loader"
+require "./packs/manifest"
+require "./packs/scaffold"
